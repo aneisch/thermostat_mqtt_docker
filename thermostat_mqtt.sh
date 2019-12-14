d5444c125717e1ff79bc2c450ed5d781a37aea99
Andrew <andrewneisch@gmail.com>
2019-12-14 14:23:14 -0600

    Exit if no message

--- thermostat_mqtt.sh
+++ thermostat_mqtt.sh
@@ -3,8 +3,11 @@
 message=$(curl -m 40 -s http://$THERMOSTAT_IP/tstat)
 
 if [[ $message == *-1* ]]
-then
-exit 1
+  then
+    exit 1
+elif [[ $message == "" ]]
+  then
+    exit 1
 fi
 
 message_fixed=$(echo $message | sed -e 's/t_heat/setpoint/' -e 's/t_cool/setpoint/')
